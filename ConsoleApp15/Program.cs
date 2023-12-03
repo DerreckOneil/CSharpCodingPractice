d5403c89567b6c1547a21edc335bbe9571d0bdd0
Derreck Oneil <derreckoneil@gmail.com>
2023-12-02 23:50:00 -0500

    Leet code problem 1903 completed.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Program
+{
+
+    static void Main()
+    {
+        Console.WriteLine("Number of Games: " + NumberOfMatches(7));
+    }
+    public static int NumberOfMatches(int n)
+    {
+        int teams = n;
+        int totalGames = 0;
+
+        while (teams > 1)
+        {
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("Even!");
+                totalGames++;
+                teams--;
+                n /= 2;
+            }
+            else
+            {
+                Console.WriteLine("Odd!");
+                totalGames++;
+                teams--;
+                n = (n - 1) / 2;
+            }
+        }
+
+        return totalGames;
+    }
+}
+
---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        string example = "4206";
+        Console.WriteLine(LargestOddNumber(example));
+    }
+    public static string LargestOddNumber(string num)
+    {
+        bool containsOdd = (num.Any(x => IsOddNumber(x))) ? true : false;
+        if (!containsOdd)
+            return "";
+
+        Console.WriteLine("NUMBER: " + num);
+        BigInteger parsedInt = 0;
+        if (num.Length == 1)
+        {
+            parsedInt = BigInteger.Parse(num);
+            if (IsOddNumber(parsedInt))
+                return num;
+            else
+                return "";
+        }
+
+        parsedInt = BigInteger.Parse(num);
+        if (IsOddNumber(parsedInt))
+            return num;
+        else
+        {
+            return LargestOddNumber(num.Substring(0, FindNearestOddFromRight(num)+1));
+        }
+
+
+
+    }
+
+    /// <summary>
+    /// Returns an index of the nearest odd from the right
+    /// </summary>
+    /// <param name="str"></param>
+    /// <returns></returns>
+    public static int FindNearestOddFromRight(string str) 
+    {
+        List<char> chars = str.ToCharArray().ToList();
+
+        int result = chars.FindLastIndex(x => !(x % 2 == 0));
+        Console.WriteLine("index? " + result);
+        return result;
+    }
+
+    public static bool IsOddNumber(BigInteger num) => !(num % 2 == 0);
+}
