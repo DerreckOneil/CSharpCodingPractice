08e3de146472c6d0f4488e4b85c18fe468fd7236
Derreck Oneil <derreckoneil@gmail.com>
2023-12-18 13:09:15 -0500

    Leet Code problem: 1913 completed

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,52 @@
+﻿
+//Definition for a binary tree node.
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        TreeNode root = new TreeNode();
+        root.val = 1;
+        root.left = new TreeNode(2, null, new TreeNode(4));
+        root.right = new TreeNode(3);
+
+        //Console.WriteLine("hold on..." + root.val + " then " + root.left.val + " then " + root.left.left.val); 
+        Console.WriteLine(Tree2str(root));
+    }
+    public static string Tree2str(TreeNode root)
+    {
+        //Solution: 1(2(4)())(3()())
+        string solution = "";
+
+        if (root == null)
+        {
+            solution = "";
+            return solution;
+        }
+        solution = root.val.ToString();
+
+        if ((root.left == null && root.right != null) || (root.left != null && root.right == null))
+            solution += $"()";
+
+        if (root.left != null)
+            solution += $"({Tree2str(root.left)})";
+        if (root.right != null)
+            solution += $"({Tree2str(root.right)})";
+        return solution;
+
+
+    }
+    public static bool HasChildren(TreeNode node) => (node.left != null && node.right != null);
+}
---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] a = { 5, 6, 2, 7, 4 };
+        Console.WriteLine("Result = " + MaxProductDifference(a));
+    }
+
+    public static int MaxProductDifference(int[] nums)
+    {
+        int result = 0;
+        Array.Sort(nums);
+
+        result =  (nums[nums.Length-1] * nums[nums.Length-2]) - (nums[0] * nums[1]);
+
+        return result;
+    }
+}
