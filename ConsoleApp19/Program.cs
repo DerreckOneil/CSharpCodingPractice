0ab84d3a3600bdd47254630ca5a18bc83b13b19a
Derreck Oneil <derreckoneil@gmail.com>
2024-01-02 19:24:56 -0500

    Leet Code remove element problem done

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+public class Program
+{
+    static void Main()
+    {
+        string a = "aaabcccd";
+        GetLengthOfOptimalCompression(a, 2);
+    }
+    public static int GetLengthOfOptimalCompression(string s, int k)
+    {
+
+        s = GetCondensedString(s);
+
+        return 0;
+    }
+    public static string GetCondensedString(string s)
+    {
+        
+        List<char> charList = s.ToCharArray().ToList();
+        List<string> strs = new List<string>();
+        
+        for(int i =0; i< charList.Count; i++)
+        {
+            int iterations = s.Count(x => x == charList[i]);
+            if (iterations > 1)
+            {
+                Console.WriteLine("This char: " + charList[i] + " has iterations: " + iterations);
+                string result = string.Concat(charList[i].ToString(),iterations.ToString());
+                strs.Add(result);
+
+                int newIndex = charList.LastIndexOf(charList[i]);
+                i = newIndex;
+                Console.WriteLine("Should the new index be: " + newIndex);
+            }
+            else
+            {
+                strs.Add(charList[i].ToString());
+            }
+        }
+
+        Console.WriteLine(PrintArray(strs.ToArray()));
+        return PrintArray(strs.ToArray());
+    }
+
+    
+    private static string PrintArray(string[] a) => string.Join("", a);
+}
---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main()
+    {
+        int[] nums = { 0, 1, 2, 2, 3, 0, 4, 2 };
+        int[] nums2 = new int[6]; 
+
+        RemoveElement(nums, 2);
+    }
+
+    public static int RemoveElement(int[] nums, int val)
+    {
+        List<int> list = nums.ToList();
+        list.RemoveAll(x => x == val);
+        nums = list.ToArray();
+        Console.WriteLine("Solution ["+ PrintArray(nums)+ "]");
+        
+        Console.WriteLine("Ans: " + nums.Length);
+        //nums = list.ToArray();
+        return nums.Length;
+    }
+    public static string PrintArray(int[] a) => String.Join(",", a);
+}
