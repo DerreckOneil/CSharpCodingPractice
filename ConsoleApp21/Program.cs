501ca3600c648881f0a2e34a15e2fa9396c26b98
Derreck Oneil <derreckoneil@gmail.com>
2024-01-29 13:57:08 -0500

    Leet Code problem Two stacks to a Queue done

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Globalization;
+
+
+public class Program
+{
+    /*
+
+    */
+    private static void Main()
+    {
+        int[] a = { 2,2 };
+        Console.WriteLine(ToString(a));
+
+        Console.WriteLine(ToString(FindErrorNums(a)));
+
+
+    }
+
+    public static int[] FindErrorNums(int[] nums)
+    {
+        Dictionary<int, int> mappings = new();
+        List<int> solution = new List<int>();
+        //bool startingFromOne = nums[0] == 1;
+        foreach (int x in nums)
+        {
+            if (mappings.ContainsKey(x))
+            {
+                solution.Add(x);
+                mappings[x]++; //Value
+                if (x == 2 && !(nums[0] == 1))
+                    solution.Add(x - 1);
+                else
+                    solution.Add(x + 1);
+
+            }
+            else
+                mappings.Add(x, 1);
+        }
+
+        foreach (KeyValuePair<int, int> pair in mappings)
+            Console.WriteLine("Key: " + pair.Key + " Value: " + pair.Value);
+
+        return solution.ToArray();
+
+    }
+
+    //public static int[] FindErrorNums(int[] nums)
+    //{
+    //    List<int> list = nums.ToList();
+    //    List<int> result = new List<int>();
+
+    //    bool startingFromOne = list[0] == 1;
+    //    Console.WriteLine(startingFromOne);
+
+    //    foreach (int x in list)
+    //    {
+    //        if (list.Count(y => y == x) > 1)
+    //        {
+    //            result.Add(x);
+    //            if (x == 2 && !startingFromOne)
+    //                result.Add(x - 1);
+    //            else
+    //                result.Add(x + 1);
+    //        }
+    //    }
+
+    //    return result.ToArray();
+    //}
+    public static string ToString(int[] a) => String.Join(", ", a);
+
+}
---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+public class MyQueue
+{
+    Stack stack1 = new Stack();
+    Stack stack2 = new Stack();
+    public MyQueue()
+    {
+
+    }
+
+    public void Push(int x)
+    {
+        if (Empty())
+        {
+            stack1.Push(x);//1
+            stack2.Push(x);
+        }
+        else
+        {
+            stack1.Push(x);
+            stack2.Clear();
+
+            foreach (int y in stack1)
+            {
+                Console.WriteLine("Pushing on y: " + y);
+                stack2.Push(y);
+            }
+        }
+    }
+    public int Pop()
+    {
+        if(stack1.Count == 0)
+            throw new Exception("Trying to pop with nothing in the stack...");
+
+        Stack temp = new();
+        object[] a = stack1.ToArray();
+
+        Console.WriteLine("The ending of this array is: "+ a[a.Length-1]);
+        for(int i = a.Length-2; i >= 0; i--)
+        {
+            Console.WriteLine("Putting in: " + a[i]);
+            temp.Push(a[i]);
+        }
+        stack1 = temp;
+        
+        return (int) stack2?.Pop();
+    }
+    public int Peek()
+    {
+        return (int) stack2?.Peek();
+    }
+    public bool Empty()
+    {
+        bool result = (stack1.Count == 0);
+        return result;
+    }
+}
+
+public class Program
+{
+    private static void Main()
+    {
+        MyQueue myQueue = new MyQueue();
+        myQueue.Push(1); // queue is: [1]
+        myQueue.Push(2); // queue is: [1, 2] (leftmost is front of the queue)
+        Console.WriteLine("Pop! num: " + myQueue.Pop());
+        myQueue.Push(5); // queue is: [2,3,4,5]
+        Console.WriteLine("Pop! num: "+myQueue.Pop()); //[3,4,5]
+        Console.WriteLine("Pop! num: "+myQueue.Pop()); //[4,5]
+        Console.WriteLine("Pop! num: "+myQueue.Pop()); //[5]
+        Console.WriteLine("Pop! num: "+myQueue.Pop()); //[]
+
+        //Console.WriteLine(myQueue.Peek()); // return 1
+        //Console.WriteLine(myQueue.Pop()); // return 1, queue is [2]
+        //Console.WriteLine(myQueue.Empty()); // return false
+    }
+
+}
